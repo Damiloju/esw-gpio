52eae8858d65346b2642a4d156b51dd7dded2bc1
Oluwadamiloju Yusuf <yusufdamiloju@gmail.com>
2022-02-02 11:22:16 +0200

    first class, following along

--- main.c
+++ main.c
@@ -50,31 +50,53 @@
 INCBIN(Header, "header.bin");
 
 // Heartbeat thread, initialize GPIO and print heartbeat messages.
-void hp_loop ()
+void hp_loop()
 {
-    #define ESWGPIO_HB_DELAY 10 // Heartbeat message delay, seconds
-    
+#define ESWGPIO_HB_DELAY 10 // Heartbeat message delay, seconds
+
     // TODO Initialize GPIO.
-    
+    CMU_ClockEnable(cmuClock_GPIO, true);
+
+    // TODO Set up the pins for the LEDs ((gpioPortB ) is PB then number is 11 for first use of pinmode set).
+    // Output mode is gpioModePushPull
+    GPIO_PinModeSet(gpioPortB, 11, gpioModePushPull, 0);
+
+    // Create a thread.
+    const osThreadAttr_t LED1_thread_attr = {.name = "LED1"};
+    osThreadNew(led_one, NULL, &LED1_thread_attr);
+
+    // GPIO_PinModeSet(gpioPortB, 12, gpioModePushPull, 0);
+    // GPIO_PinModeSet(gpioPortA, 5, gpioModePushPull, 0);
+
     for (;;)
     {
-        osDelay(ESWGPIO_HB_DELAY*osKernelGetTickFreq());
+        osDelay(ESWGPIO_HB_DELAY * osKernelGetTickFreq());
         info1("Heartbeat");
     }
 }
 
 // TODO LED toggle thread.
+void led_one()
+{
+    for (;;)
+    {
+        // TODO Toggle LED1.
+        GPIO_PinOutToggle(gpioPortB, 11);
+        info1("LED1 esw-gpio");
+        osDelay(300);
+    }
+}
 
 // TODO Button interrupt thread.
 
-int logger_fwrite_boot (const char *ptr, int len)
+int logger_fwrite_boot(const char *ptr, int len)
 {
     fwrite(ptr, len, 1, stdout);
     fflush(stdout);
     return len;
 }
 
-int main ()
+int main()
 {
     PLATFORM_Init();
 
@@ -82,13 +104,13 @@ int main ()
     RETARGET_SerialInit();
     log_init(BASE_LOG_LEVEL, &logger_fwrite_boot, NULL);
 
-    info1("ESW-GPIO "VERSION_STR" (%d.%d.%d)", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+    info1("ESW-GPIO " VERSION_STR " (%d.%d.%d)", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
 
     // Initialize OS kernel.
     osKernelInitialize();
 
     // Create a thread.
-    const osThreadAttr_t hp_thread_attr = { .name = "hp" };
+    const osThreadAttr_t hp_thread_attr = {.name = "hp"};
     osThreadNew(hp_loop, NULL, &hp_thread_attr);
 
     if (osKernelReady == osKernelGetState())
@@ -105,5 +127,6 @@ int main ()
         err1("!osKernelReady");
     }
 
-    for(;;);
+    for (;;)
+        ;
 }
